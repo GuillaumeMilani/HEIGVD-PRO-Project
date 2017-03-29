e5d2d76a83ec158faac1b3711e9633803ead5654
Mathieu <mathieu.monteverde@gmail.com>
2017-03-29 15:36:04 +0200

    Added first methods to Workspace

---
+++ GEMMS/src/main/java/ch/heigvd/workspace/Workspace.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.heigvd.workspace;
+
+import java.util.List;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.image.WritableImage;
+
+/**
+ *
+ * @author mathieu
+ */
+public class Workspace extends Group {
+   /**
+    * 
+    * @param node 
+    */
+   public void addLayer(Node node) {
+      
+   }
+   
+   /**
+    * 
+    * @param node 
+    */
+   public void removeLayer(Node node) {
+      
+   }
+   
+   /**
+    * 
+    * @return 
+    */
+   public List<Node> getLayers() {
+      return null;
+   }
+   
+   /**
+    * 
+    * @param params
+    * @param image
+    * @return 
+    */
+   @Override
+   public WritableImage snapshot(SnapshotParameters params, WritableImage image) {
+      return null;
+   }
+   
+   /**
+    * 
+    * @param z 
+    */
+   public void zoom(int z) {
+      
+   }
+   
+   /**
+    * 
+    * @param x
+    * @param y
+    */
+   public void move(int x, int y) {
+      
+   }
+   
+   /**
+    * 
+    */
+   public void crop() {
+      
+   }
+   
+}

09b1ed7971324f8a423cb484c30ec6c5c5a0254c
Mathieu <mathieu.monteverde@gmail.com>
2017-04-26 14:41:38 +0200

    Implementation of Workspace (constructor, move & zooom)

--- GEMMS/src/main/java/ch/heigvd/workspace/Workspace.java
+++ GEMMS/src/main/java/ch/heigvd/workspace/Workspace.java
@@ -8,14 +8,41 @@ package ch.heigvd.workspace;
 import java.util.List;
 import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.scene.SnapshotParameters;
-import javafx.scene.image.WritableImage;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 /**
  *
  * @author mathieu
  */
-public class Workspace extends Group {
+public class Workspace extends AnchorPane {
+   
+   /**
+    * Constructor for a new instance of Workspace. The Workspace extends a Pane
+    * which represents the working area of the document. It sets its initial 
+    * position at the center of the containing pane.
+    * @param width the width of the Workspace (according to the document needs)
+    * @param height the height of the Workspace (according to the document needs)
+    * @param pane the application pane in which the Workspace is to be added.
+    */
+   public Workspace(int width, int height, Pane pane) {
+      // Explciit call to parent constructor
+      super();
+      
+      // Set the workspace Pane position to be at the center of pane
+      int posX = (int) ((pane.getPrefWidth() - width) / 2);
+      int posY = (int) ((pane.getPrefHeight() - height) / 2);
+      setLayoutX(posX);
+      setLayoutY(posY);
+      
+      // Set preferredSize
+      setPrefSize(width, height);
+      setMaxSize(width, height);
+      setMinSize(width, height);
+      
+      // Give the CSS ID
+      setId("workspacePane");
+   }
    
    private Node currentLayer;
    
@@ -56,17 +83,19 @@ public class Workspace extends Group {
     *
     * @param z
     */
-   public void zoom(int z) {
-
+   public void zoom(double factor) {
+      setScaleX(getScaleX() * factor);
+      setScaleY(getScaleY() * factor);
    }
 
    /**
-    *
-    * @param x
-    * @param y
+    * Translates the Workspace Pane container by a (x, y)  translation vector.
+    * @param x the x coordinate of the translation
+    * @param y the y coordinate of the translation
     */
-   public void move(int x, int y) {
-
+   public void move(double x, double y) {
+      setTranslateX(getTranslateX() + x);
+      setTranslateY(getTranslateY() + y);
    }
 
    /**

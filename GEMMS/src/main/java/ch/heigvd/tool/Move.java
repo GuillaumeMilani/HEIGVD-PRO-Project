2d25bf94bb1b74fcef13e46161e4c26a7eca5124
ripwin <sathiya.kirushnapillai@gmail.com>
2017-05-03 19:02:53 +0200

    Change package

---
+++ GEMMS/src/main/java/ch/heigvd/tool/Move.java
@@ -0,0 +1,27 @@
+package ch.heigvd.tool;
+
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+
+public class Move implements Tool {
+
+	public Move(Scene scene) {
+		scene.setCursor(Cursor.DEFAULT);
+	}
+	
+	@Override
+	public void mousePressed(double x, double y) {
+		
+	}
+
+	@Override
+	public void mouseDragged(double x, double y) {
+
+	}
+
+	@Override
+	public void mouseReleased(double x, double y) {
+
+	}
+
+}
---
+++ GEMMS/src/main/java/ch/heigvd/tool/Tool.java
@@ -0,0 +1,9 @@
+package ch.heigvd.tool;
+
+
+public interface Tool {
+    void mousePressed(double x, double y);
+    void mouseDragged(double x, double y);
+    void mouseReleased(double x, double y);
+}
+

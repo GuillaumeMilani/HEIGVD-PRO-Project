0c840aed7e1795fce7e975a1787a4f70a8b893fe
ripwin <sathiya.kirushnapillai@gmail.com>
2017-05-10 14:28:41 +0200

    Delete duplicate class

--- GEMMS/src/main/java/ch/heigvd/gemms/Tool.java
+++
@@ -1,9 +0,0 @@
-package ch.heigvd.gemms;
-
-
-public interface Tool {
-    void mousePressed(double x, double y);
-    void mouseDragged(double x, double y);
-    void mouseReleased(double x, double y);
-}
-

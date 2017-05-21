24ebcb39590645db97950f79f5a01b8b65bd47f2
Michael Spierer <evondom@hotmail.com>
2017-05-21 14:22:39 +0200

    marche mais pas super sur les bords

--- GEMMS/src/main/java/ch/heigvd/tool/BucketFill.java
+++ GEMMS/src/main/java/ch/heigvd/tool/BucketFill.java
@@ -11,8 +11,8 @@ import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
 
 /**
  * Created by Michael on 21.05.2017.
@@ -45,10 +45,10 @@ public class BucketFill implements Tool {
     }
 
 
-    protected void drawPixel(int x, int y, GraphicsContext gc) {
+    private void drawPixel(int x, int y, GraphicsContext gc) {
         gc.setFill(colorToFillWith);
-        int size = 1;
-        gc.fillOval(x ,y, size   , size);
+        int size = 10;
+        gc.fillOval(x , y , size, size);
     }
 
 
@@ -56,50 +56,46 @@ public class BucketFill implements Tool {
         WritableImage wi = new WritableImage((int) canvas.getBoundsInParent().getWidth(), (int) canvas.getBoundsInParent().getHeight());
         WritableImage snapshot = canvas.snapshot(new SnapshotParameters(), wi);
         PixelReader pr = snapshot.getPixelReader();
-        PixelWriter pw = canvas.getGraphicsContext2D().getPixelWriter();
+        PixelWriter pw = snapshot.getPixelWriter();
 
         Color colorBegin = pr.getColor((int)begin.getX(),(int)begin.getY());
-        Stack<Point2D> stack = new Stack<>();
-        stack.push(begin);
+        LinkedList<Point2D> fifo = new  LinkedList<>();
+        fifo.add(begin);
 
         if(colorToFillWith.equals(colorBegin)){
             return;
         }
 
-        //tant que la stack n'est pas vide, on ajoute tous les voisins de la meme couleurs et on colorit
-        while (!stack.isEmpty()) {
-            System.out.println("size: " + stack.size());
-            Point2D currentPoint = stack.pop();
+
+        //tant que la fifo n'est pas vide, on ajoute tous les voisins de la meme couleurs et on colorit
+        while (!fifo.isEmpty() ) {
+            Point2D currentPoint = fifo.removeFirst();
             int currentPointX = (int) currentPoint.getX();
             int currentPointY = (int) currentPoint.getY();
             Color pixelColor = pr.getColor(currentPointX,currentPointY);
 
             if (isSameColor(pixelColor,colorBegin)) { //Si la couleur du départ et celle de maintenant sont les memes on ajoutes
-                continue;
+              continue;
             }
 
-
-           // drawPixel(currentPointX,currentPointY,canvas.getGraphicsContext2D());
-
             pw.setColor(currentPointX, currentPointY, colorToFillWith);
-
-            pushIntoStack(stack, currentPointX - 1, currentPointY - 1, wi);
-            pushIntoStack(stack, currentPointX - 1, currentPointY, wi);
-            pushIntoStack(stack, currentPointX - 1, currentPointY + 1, wi);
-            pushIntoStack(stack, currentPointX, currentPointY + 1, wi);
-            pushIntoStack(stack, currentPointX + 1, currentPointY + 1, wi);
-            pushIntoStack(stack, currentPointX + 1, currentPointY, wi);
-            pushIntoStack(stack, currentPointX + 1, currentPointY - 1, wi);
-            pushIntoStack(stack, currentPointX, currentPointY - 1, wi);
+            pushIntoFifo(fifo, currentPointX - 1, currentPointY - 1, canvas, pr,colorBegin);
+            pushIntoFifo(fifo, currentPointX - 1, currentPointY, canvas, pr,colorBegin);
+            pushIntoFifo(fifo, currentPointX - 1, currentPointY + 1, canvas, pr,colorBegin);
+            pushIntoFifo(fifo, currentPointX, currentPointY + 1, canvas, pr,colorBegin);
+            pushIntoFifo(fifo, currentPointX, currentPointY - 1, canvas, pr,colorBegin);
+            pushIntoFifo(fifo, currentPointX + 1, currentPointY + 1, canvas, pr,colorBegin);
+            pushIntoFifo(fifo, currentPointX + 1, currentPointY, canvas, pr,colorBegin);
+            pushIntoFifo(fifo, currentPointX + 1, currentPointY - 1, canvas, pr,colorBegin);
 
 
         }
-        //System.out.println("FINI");
+        canvas.getGraphicsContext2D().drawImage(snapshot,0,0);
     }
 
-    private void pushIntoStack(Stack<Point2D> stack, int x, int y, WritableImage wi) {
-        if (x > 0 || x < wi.getWidth() || y > 0 || y < wi.getHeight()) {
-            stack.push(new Point2D(x, y));
+    private void pushIntoFifo( LinkedList<Point2D> fifo, int x, int y, Node wi,PixelReader pr, Color colored) {
+        if (x > 0 && x < wi.getBoundsInParent().getWidth() && y > 0 && y <  wi.getBoundsInParent().getHeight()) {
+        fifo.add(new Point2D(x, y));
         }
     }
 
@@ -119,24 +115,6 @@ public class BucketFill implements Tool {
         return Math.abs(a - b) < gamma;
     }
 
-
-    /**
-     * Set the color of the brush.
-     *
-     * @param color the new Color
-     */
-    public void setColor(Color color) {
-        this.colorToFillWith = color;
-    }
-
-    /**
-     * Get the current brush color.
-     *
-     * @return the color of the brush
-     */
-    public Color getColor() {
-        return colorToFillWith;
-    }
 
 
     @Override

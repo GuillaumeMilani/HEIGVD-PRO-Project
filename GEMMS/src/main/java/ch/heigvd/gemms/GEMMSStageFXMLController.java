aaaacd9376b34f4b6bf7a5ebcbf8afa7210b1963
ripwin <sathiya.kirushnapillai@gmail.com>
2017-05-21 20:25:06 +0200

    Clean controller import

--- GEMMS/src/main/java/ch/heigvd/gemms/GEMMSStageFXMLController.java
+++ GEMMS/src/main/java/ch/heigvd/gemms/GEMMSStageFXMLController.java
@@ -5,11 +5,17 @@ import ch.heigvd.dialog.NewDocument;
 import ch.heigvd.dialog.NewDocumentDialog;
 import ch.heigvd.dialog.OpenDocumentDialog;
 import ch.heigvd.dialog.ResizeDialog;
-import ch.heigvd.layer.GEMMSText;
-import ch.heigvd.layer.IGEMMSNode;
-import ch.heigvd.tool.*;
 
+import ch.heigvd.layer.GEMMSText;
+import ch.heigvd.layer.GEMMSCanvas;
+import ch.heigvd.layer.IGEMMSNode;
+import ch.heigvd.layer.GEMMSImage;
+
+import ch.heigvd.tool.*;
 import ch.heigvd.tool.settings.ToolColorSettings;
+import ch.heigvd.tool.settings.ToolFontSettings;
+import ch.heigvd.tool.settings.ToolSettingsContainer;
+import ch.heigvd.tool.settings.ToolSizeSettings;
 
 import ch.heigvd.workspace.Workspace;
 
@@ -18,58 +24,32 @@ import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.URL;
 import java.util.*;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.embed.swing.SwingFXUtils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Button;
 import javafx.scene.image.*;
-import javafx.scene.image.Image;
 import javafx.scene.input.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
-import ch.heigvd.layer.GEMMSCanvas;
-import ch.heigvd.layer.GEMMSImage;
-
-
-import ch.heigvd.tool.Brush;
-import ch.heigvd.tool.ColorSet;
-import ch.heigvd.tool.Eraser;
-import ch.heigvd.tool.EyeDropper;
-import ch.heigvd.tool.Selection;
-import ch.heigvd.tool.TextTool;
-
-import ch.heigvd.tool.settings.ToolColorSettings;
-import ch.heigvd.tool.settings.ToolFontSettings;
-import java.util.List;
-import ch.heigvd.tool.settings.ToolSettingsContainer;
-import ch.heigvd.tool.settings.ToolSizeSettings;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Optional;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
-import javafx.event.ActionEvent;
-import javafx.geometry.Point3D;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
 import javafx.scene.effect.ColorAdjust;
-import javafx.scene.effect.Effect;
 import javafx.scene.effect.SepiaTone;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Paint;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.VBox;
@@ -78,8 +58,7 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Rotate;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
-import javafx.util.Pair;
-import javafx.scene.text.Font;
+
 
 public class GEMMSStageFXMLController implements Initializable {
 

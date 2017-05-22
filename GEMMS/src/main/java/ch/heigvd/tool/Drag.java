4649b2f606951a88fc39f06c7cf22a843d55bc29
Michael Spierer <evondom@hotmail.com>
2017-05-22 15:37:29 +0200

    drag marche

--- GEMMS/src/main/java/ch/heigvd/tool/Drag.java
+++ GEMMS/src/main/java/ch/heigvd/tool/Drag.java
@@ -4,18 +4,14 @@ import ch.heigvd.workspace.Workspace;
 import javafx.geometry.Point3D;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
-import javafx.scene.transform.NonInvertibleTransformException;
-import javafx.scene.transform.Transform;
 
-import java.util.List;
 
-/**
- * Created by Michael on 17.05.2017.
- */
 public class Drag implements Tool{
-    private double mouseX;
-    private double mouseY;
+
     private Workspace workspace;
+
+    private double lastX;
+    private double lastY;
 
     public Drag(Workspace w){
         this.workspace = w;
@@ -24,56 +20,25 @@ public class Drag implements Tool{
     @Override
     public void mousePressed(double x, double y) {
         workspace.setCursor(Cursor.CLOSED_HAND);
-        mouseX = x;
-        mouseY = y;
+
+        lastX = x;
+        lastY = y;
     }
 
     @Override
     public void mouseDragged(double x, double y) {
 
-        List<Node> layers = workspace.getCurrentLayers();
+        double offsetX = x - lastX;
+        double offsetY = y - lastY;
 
+        for(Node n : workspace.getLayers()) {
 
-
-        for (Node node : layers) {
-            //Essaie3
-            Point3D point = PositionMapper.convert(node, x, y, 0);
-            Point3D point2 = PositionMapper.convert(node, mouseX, mouseY, 0);
-
-            double newX =x - point2.getX();
-            double newY = y - point2.getY();
-            node.setTranslateX(newX + point.getX());
-            node.setTranslateY(newY + point.getY());
-
-            //Essaie 2
-//            Point3D point = PositionMapper.convert(node, x, y, 0);
-//            Point3D point2 = PositionMapper.convert(node, mouseX, mouseY, 0);
-//
-//            double newX = point.getX() - point2.getX();
-//            double newY = point.getY() - point2.getY();
-//            node.setTranslateX(newX + node.getTranslateX());
-//            node.setTranslateY(newY + node.getTranslateY());
-//
-            //Essaie 1
-//            Point3D p = new Point3D(newX, newY, 0);
-//
-//
-//            for (Transform t : node.getTransforms()) {
-//
-//               try {
-//                    p = t.createInverse().inverseDeltaTransform(p);
-//                } catch (NonInvertibleTransformException ex) {
-//                   ex.printStackTrace();
-//                }
-//            }
-//            node.setTranslateX(p.getX() + node.getTranslateX());
-//            node.setTranslateY(p.getY() + node.getTranslateY());
-
+            n.setTranslateX(n.getTranslateX() + offsetX);
+            n.setTranslateY(n.getTranslateY() + offsetY);
         }
 
-        mouseX = x;
-        mouseY = y;
-
+        lastX = x;
+        lastY = y;
     }
 
     @Override
@@ -81,6 +46,4 @@ public class Drag implements Tool{
         workspace.setCursor(Cursor.DEFAULT);
 
     }
-
-
 }

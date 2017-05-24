f11e8c40be1c3993fea85b59b5b0c64a0e3cc07b
Michael Spierer <evondom@hotmail.com>
2017-05-24 14:57:54 +0200

    marche mais pas aligné par rapport au centre

--- GEMMS/src/main/java/ch/heigvd/tool/Drag.java
+++ GEMMS/src/main/java/ch/heigvd/tool/Drag.java
@@ -4,6 +4,7 @@ import ch.heigvd.workspace.Workspace;
 import javafx.geometry.Point3D;
 import javafx.scene.Cursor;
 import javafx.scene.Node;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Drag implements Tool{
@@ -12,17 +13,25 @@ public class Drag implements Tool{
 
     private double lastX;
     private double lastY;
+    private boolean isAlignementActive;
+    private final double ALIGNEMENT_DELTA = 20;
 
     public Drag(Workspace w){
         this.workspace = w;
-
+        isAlignementActive = true;
     }
+
     @Override
     public void mousePressed(double x, double y) {
         workspace.setCursor(Cursor.CLOSED_HAND);
 
         lastX = x;
         lastY = y;
+    }
+
+    private void setPosition(double x, double y, Node n){
+        n.setTranslateX(x);
+        n.setTranslateY(y);
     }
 
     @Override
@@ -33,8 +42,28 @@ public class Drag implements Tool{
 
         for(Node n : workspace.getCurrentLayers()) {
 
-            n.setTranslateX(n.getTranslateX() + offsetX);
-            n.setTranslateY(n.getTranslateY() + offsetY);
+            if(isAlignementActive){
+                AnchorPane anchorPane = workspace.getLayerTool();
+                double middleWorkspaceWidth = workspace.width()/2;
+                double middleWorkspaceHeight = workspace.height()/2;
+                double middleNodeWidth = (n.getBoundsInParent().getMinX() + n.getBoundsInParent().getMaxX())/2;
+                double middleNodeHeight =(n.getBoundsInParent().getMinY() + n.getBoundsInParent().getMaxY())/2 ;
+
+                if(Math.abs(x - middleWorkspaceWidth) < ALIGNEMENT_DELTA){
+                    setPosition(middleWorkspaceWidth,y,n);
+
+                }
+                if(Math.abs(y - middleWorkspaceHeight) < ALIGNEMENT_DELTA){
+                    setPosition(x,middleWorkspaceHeight,n);
+
+                }else{
+                    n.setTranslateX(n.getTranslateX() + offsetX);
+                    n.setTranslateY(n.getTranslateY() + offsetY);
+                }
+            }else {
+                n.setTranslateX(n.getTranslateX() + offsetX);
+                n.setTranslateY(n.getTranslateY() + offsetY);
+            }
         }
 
         lastX = x;
